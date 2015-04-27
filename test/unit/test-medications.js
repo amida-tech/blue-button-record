5340f4fbaf75719f844c1aa76cd7673c1fc8e482
Jacob Sachs <jacob.s.sachs@gmail.com>
2015-04-27 11:37:10 -0500

    add getAll and delete medications

--- test/unit/test-medications.js
+++ test/unit/test-medications.js
@@ -91,61 +91,70 @@ describe('medications.js methods', function () {
 
     });
 
-    // it('get medications', function (done) {
-    //     //record.
-    //     record.getAllMedications("username", function (err, data) {
-    //
-    //         expect(data).to.exist;
-    //         expect(data).to.have.length(2);
-    //
-    //         done();
-    //
-    //     });
-    //
-    // });
+    it('get medications', function (done) {
+        //record.
+        record.getAllMedications("username", function (err, data) {
 
-    // it('add and delete medication', function (done) {
-    //     //record.
-    //
-    //     record.getAllNotes("username", function (err, data) {
-    //
-    //         expect(data).to.exist;
-    //         expect(data).to.have.length(2);
-    //
-    //         record.addNote("username", "section", "entry", "note_to_be deleted", function (err, data) {
-    //
-    //             expect(data.datetime).to.exist;
-    //             expect(data._id).to.exist;
-    //             expect(data.section).to.exist;
-    //             expect(data.entry).to.exist;
-    //             expect(data.note).to.equal("note_to_be deleted");
-    //
-    //             var id = data._id;
-    //
-    //             record.getAllNotes("username", function (err, data) {
-    //
-    //                 expect(data).to.exist;
-    //                 expect(data).to.have.length(3);
-    //                 //console.log(data);
-    //
-    //                 record.deleteNote("username", id, function (err, data) {
-    //                     expect(err).to.be.null;
-    //
-    //                     record.getAllNotes("username", function (err, data) {
-    //
-    //                         expect(data).to.exist;
-    //                         expect(data).to.have.length(2);
-    //                         //console.log(data);
-    //
-    //                         done();
-    //                     });
-    //                 });
-    //             });
-    //         });
-    //
-    //     });
-    //
-    // });
+            expect(data).to.exist;
+            expect(data).to.have.length(2);
+
+            done();
+
+        });
+
+    });
+
+    it('add and delete medication', function (done) {
+        //record.
+
+        record.getAllMedications("username", function (err, data) {
+
+            expect(data).to.exist;
+            expect(data).to.have.length(2);
+
+            var med = {
+                "identifiers": [{
+                    "identifier": "cdbd33f0-6cde-11db-9fe1-0800200c9a68"
+                }],
+                "status": "Completed",
+                "sig": "Delete me!",
+                "product": {
+                    "identifiers": [{
+                        "identifier": "2a620155-9d11-439e-92b3-5d9815ff4ee8"
+                    }],
+                    "unencoded_name": "Proventil3 HFA",
+                }
+            };
+
+            record.addMedication("username", med, function (err, data) {
+                expect(data._id).to.exist;
+                expect(data.pat_key).to.exist;
+                expect(data.data.sig).to.equal("Delete me!");
+
+                var id = data._id;
+
+                record.getAllMedications("username", function (err, data) {
+
+                    expect(data).to.exist;
+                    expect(data).to.have.length(3);
+
+                    record.deleteMedication("username", id, function (err, data) {
+                        expect(err).to.be.null;
+
+                        record.getAllMedications("username", function (err, data) {
+
+                            expect(data).to.exist;
+                            expect(data).to.have.length(2);
+
+                            done();
+                        });
+                    });
+                });
+            });
+
+        });
+
+    });
 
     after(function (done) {
         record.clearDatabase(function (err) {
