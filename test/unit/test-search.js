cec427d5cdab53093438f7e86c9e727fb8977b0e
Afsin Ustundag <afsin.ustundag@us.pwc.com>
2015-05-23 22:26:52 -0400

    basic paging is complete

--- test/unit/test-search.js
+++ test/unit/test-search.js
@@ -17,11 +17,29 @@ describe('search.js', function () {
 
     refmodel.prepareConnection({
         dbName: 'search',
-        fhir: true
+        fhir: true,
+        maxSearch: 5
     }, context)();
 
     it('add sources', function (done) {
         refmodel.addSourcesPerPatient(context, [1, 1, 1, 1], done);
+    });
+
+    it('search testallergies when empty', function (done) {
+        var itself = this;
+        var searchSpec = {
+            section: 'testallergies',
+            patientInfo: true
+        };
+        search.search(context.dbinfo, searchSpec, function (err, result, searchInfo) {
+            expect(searchInfo).to.exist;
+            expect(searchInfo.searchId).not.to.exist;
+            expect(searchInfo.page).to.equal(0);
+            expect(searchInfo.pageSize).to.equal(5);
+            expect(searchInfo.total).to.equal(0);
+            expect(result).to.have.length(0);
+            done();
+        });
     });
 
     var expectedPatData = _.range(4).map(function (ptIndex) {
@@ -47,8 +65,13 @@ describe('search.js', function () {
             query: {},
             patientInfo: false
         };
-        search.search(context.dbinfo, searchSpec, function (err, result) {
+        search.search(context.dbinfo, searchSpec, function (err, result, searchInfo) {
             expect(result).to.have.length(4);
+            expect(searchInfo).to.exist;
+            expect(searchInfo.searchId).not.to.exist;
+            expect(searchInfo.page).to.equal(0);
+            expect(searchInfo.pageSize).to.equal(5);
+            expect(searchInfo.total).to.equal(4);
             var resultData = result.map(function (r) {
                 return r.data;
             });
@@ -67,44 +90,67 @@ describe('search.js', function () {
     expectedAllergyData = _.flatten(expectedAllergyData);
     expectedAllergyData.reverse();
 
-    it('search all testallergies', function (done) {
+    var searchIdAllergies;
+
+    var verifyTestAllergies = function (actual, offset) {
+        var itself = this;
+        var count = 0;
+        _.range(actual.length).forEach(function (index) {
+            var ptIndex = Math.floor((offset + index) / 8);
+            var e = actual[index];
+            var ptKey = util.format('pat%s', 3 - ptIndex);
+            expect(e._ptKey).to.be.equal(ptKey);
+            expect(e._pt).to.exist;
+            expect(e._pt.reference).to.be.equal(patientIds[ptIndex]);
+            ++count;
+        }, itself);
+        expect(count).to.not.equal(0);
+        expect(count).to.equal(actual.length);
+        var actualData = actual.map(function (r) {
+            return r.data;
+        });
+        var expectedData = expectedAllergyData.slice(offset, offset + actual.length);
+        expect(actualData).to.deep.equal(expectedData);
+    };
+
+    it('search testallergies page:0 (initial call)', function (done) {
+        var itself = this;
         var searchSpec = {
             section: 'testallergies',
-            query: {},
             patientInfo: true
         };
-        var itself = this;
-        search.search(context.dbinfo, searchSpec, function (err, result) {
-            expect(result).to.have.length(32);
-            var count = 0;
-            _.range(4).forEach(function (ptIndex) {
-                _.range(8).forEach(function (allergyIndex) {
-                    var e = result[ptIndex * 8 + allergyIndex];
-                    var ptKey = util.format('pat%s', 3 - ptIndex);
-                    expect(e._ptKey).to.be.equal(ptKey);
-                    expect(e._pt).to.exist;
-                    expect(e._pt.reference).to.be.equal(patientIds[ptIndex]);
-                    ++count;
-                }, itself);
-            }, itself);
-            expect(count).to.equal(32);
-            var resultData = result.map(function (r) {
-                return r.data;
-            });
-            expect(resultData).to.deep.equal(expectedAllergyData);
+        search.search(context.dbinfo, searchSpec, function (err, result, searchInfo) {
+            expect(searchInfo).to.exist;
+            expect(searchInfo.searchId).to.exist;
+            expect(searchInfo.page).to.equal(0);
+            expect(searchInfo.pageSize).to.equal(5);
+            expect(searchInfo.total).to.equal(32);
+            expect(result).to.have.length(5);
+            searchIdAllergies = searchInfo.searchId;
+            verifyTestAllergies.call(itself, result, 0);
             done();
         });
     });
 
-    after(function (done) {
-        context.dbinfo.db.dropDatabase(function (err) {
-            if (err) {
-                done(err);
-            } else {
-                context.dbinfo.connection.close(function (err) {
-                    done(err);
-                });
-            }
+    _.range(0, 7).forEach(function (index) {
+        it('search testallergies page:' + index, function (done) {
+            var itself = this;
+            var searchSpec = {
+                searchId: searchIdAllergies,
+                section: 'testallergies',
+                patientInfo: true,
+                page: index
+            };
+            search.search(context.dbinfo, searchSpec, function (err, result, searchInfo) {
+                expect(searchInfo).to.exist;
+                expect(searchInfo.searchId).to.equal(searchIdAllergies);
+                expect(searchInfo.page).to.equal(index);
+                expect(searchInfo.pageSize).to.equal(5);
+                expect(searchInfo.total).to.equal(32);
+                expect(result).to.have.length(index === 6 ? 2 : 5);
+                verifyTestAllergies.call(itself, result, index * 5);
+                done();
+            });
         });
-    });
+    }, self);
 });
